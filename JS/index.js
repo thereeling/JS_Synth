b0f5732c0b05ee2ad3c7f00b9d64d659cdaeaf8d
Nick Giuliani <nickg@pop-os.localdomain>
2022-07-30 14:46:21 -0400

    added effects to synth, will create a way for user to change effects in the webpage

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
-const synth = new Tone.PolySynth().toDestination();
+const crusher = new Tone.BitCrusher(4).toDestination();
+const synth = new Tone.Synth().connect(crusher);
 const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 var html = '';
 for (let octave = 0; octave < 2; octave++) {
@@ -9,16 +10,24 @@ for (let octave = 0; octave < 2; octave++) {
         if (note == 'E' || note == 'B')
             hasSharp = false;
 
-        html += `<div class='whitenote' onmousedown='noteDown(this)' onmouseup='noteUp(this, false)' onmouseleave='noteUp(this, false)' data-note='${note + (octave + 4)}'>`
+        html += `<div class='whitenote' onmousedown='noteDown(this)' onmouseup='noteUp(this, false)' onmouseleave='noteUp(this, false)' data-note='${note + (octave + 1)}'>`
 
         if (hasSharp) {
-            html += `<div class='blacknote' onmousedown='noteDown(this, true)' onmouseup='noteUp(this, true)' onmouseleave='noteUp(this, true)' data-note='${note + '#' + (octave + 4)}'></div>`
+            html += `<div class='blacknote' onmousedown='noteDown(this, true)' onmouseup='noteUp(this, true)' onmouseleave='noteUp(this, true)' data-note='${note + '#' + (octave + 1)}'></div>`
         }
 
         html += `</div>`
     }
 
 }
+
+const distortion = new Tone.Distortion(0.5).toDestination();
+const feedbackDelay = new Tone.FeedbackDelay(0.5, 0.75).toDestination();
+const filter = new Tone.Filter(1000, "lowpass").toDestination();
+filter.frequency.rampTo(50, 1);
+synth.connect(distortion);
+synth.connect(feedbackDelay);
+synth.connect(filter);
 
 
 
